ec9b88494c43df38504ce619664b945319adab6b
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2019-07-24 13:37:26 +0700

    :construction: setup Rest API

---
+++ app/src/main/java/com/yoesuv/mycoroutines/menu/models/ListPlaceModel.kt
@@ -0,0 +1,15 @@
+package com.yoesuv.mycoroutines.menu.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class ListPlaceModel (
+    @SerializedName("status_code") @Expose val statusCode: Int,
+    @SerializedName("data") @Expose val data: List<PlaceModel>
+) {
+    data class PlaceModel (
+        @SerializedName("name") @Expose val name: String,
+        @SerializedName("location") @Expose val location: String,
+        @SerializedName("image") @Expose val image: String
+    )
+}
---
+++ app/src/main/java/com/yoesuv/mycoroutines/networks/ServiceFactory.kt
@@ -0,0 +1,33 @@
+package com.yoesuv.mycoroutines.networks
+
+import android.os.Build
+import com.yoesuv.mycoroutines.BuildConfig
+import com.yoesuv.mycoroutines.data.AppConstants
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ServiceFactory {
+
+    fun create(): RestAPI {
+        val logging = HttpLoggingInterceptor()
+        if (BuildConfig.DEBUG) {
+            logging.level = HttpLoggingInterceptor.Level.BODY
+        } else {
+            logging.level = HttpLoggingInterceptor.Level.NONE
+        }
+
+        val clientBuilder = OkHttpClient.Builder()
+        clientBuilder.addInterceptor(logging)
+        clientBuilder.connectTimeout(AppConstants.TIME_OUT, TimeUnit.SECONDS)
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(AppConstants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(clientBuilder.build())
+            .build()
+        return retrofit.create(RestAPI::class.java)
+    }
+}
